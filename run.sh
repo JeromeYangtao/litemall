207430be2a710dddae63ed100083ad6873e2974a
mianyun.yt <mianyun.yt@alibaba-inc.com>
2020-04-08 10:16:44 +0800

    feat:开发命令封装

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#本地开发
+#java -Dfile.encoding=UTF-8 -jar litemall-all/target/litemall-all-0.1.0-exec.jar
+
+#cd litemall-admin && npm run dev
+
+#部署
+#nohup java -jar /root/litemall/deploy/litemall/litemall.jar &
